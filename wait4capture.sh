340360f237eba6df405c9fed3e2a2db4d95fed22
s-light <github@s-light.eu>
2017-04-27 22:02:23 +0200

    added killall gvfsd-gphoto2 to fix raspberrypi

--- wait4capture.sh
+++ wait4capture.sh
@@ -1,3 +1,11 @@
-# activate gphoto2
 
-gphoto2 --capture-tethered --hook-script=photobooth_handler.py --filename="./captured/photo_booth-%Y%m%d-%H%M%S.%C" --force-overwrite
+# optional stop gphotofs related things.
+# this is needed for raspberrypi to free up access to the camera
+killall gvfsd-gphoto2
+
+# activate gphoto2
+gphoto2 \
+    --capture-tethered \
+    --hook-script=photobooth_handler.py \
+    --filename="./captured/photo_booth-%Y%m%d-%H%M%S.%C" \
+    --force-overwrite
